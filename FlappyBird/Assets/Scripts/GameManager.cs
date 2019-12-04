5b17b6ae137181495d7ec1b650ca7a1f2b952c7a
luca-bruno <56382257+luca-bruno@users.noreply.github.com>
2019-12-04 19:53:24 +0100

    Score, Replay button finished

--- FlappyBird/Assets/Scripts/GameManager.cs
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -6,27 +6,31 @@ public class GameManager : MonoBehaviour
 {
 
     public GameObject startCanvas;
+    public GameObject scoreCanvas;
     public GameObject gameOverCanvas;
     private bool started = false;
 
     private void Start() {
         Time.timeScale = 0;
         startCanvas.SetActive(true);
+        scoreCanvas.SetActive(false);
         gameOverCanvas.SetActive(false);
     }    
 
     void Update() {
-        do {
+        if(started == false) {
             if(Input.GetMouseButtonDown(0)){
-            Time.timeScale = 1;
-            startCanvas.SetActive(false);
-            started = true;
+                Time.timeScale = 1;
+                startCanvas.SetActive(false);
+                scoreCanvas.SetActive(true);
+                started = true;
             }
-    } while (started == false);
+    }
 }
 
     public void GameOver(){
         gameOverCanvas.SetActive(true);
+        scoreCanvas.SetActive(false);
         Time.timeScale = 0;
     }
 }
